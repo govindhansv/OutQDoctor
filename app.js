325300b36beb97376788f40a642832d7c1940386
gokulhansv <gokulhansv@gmail.com>
2023-04-07 16:28:17 +0530

    new

--- app.js
+++ app.js
@@ -66,19 +66,19 @@ app.use('/checkouts', checkoutRouter);
 app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', { error: true });
 });
 
 module.exports = app;
